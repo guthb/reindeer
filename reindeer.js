39576ae8c5638da773c093a98394dd2667fc3f00
Bradley Guthrie <guppy96@gmail.com>
2016-01-30 15:31:40 -0600

    removed array

--- reindeer.js
+++ reindeer.js
@@ -3,17 +3,16 @@ var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donne
 
 
 var hohohoElement = document.getElementById("reindeer");
-console.log("div",hohohoElement );
+
 //loop through all the reindeer in the array, and add the name of the reindeer to the single HTML <div> element provided. 
 //The name of the reindeer should be prepended with the corresponding color from the other array.
 
 var data = []
 
 for (var i = 0; i <= reindeer.length; i++) {
-    data[i] = colors[i] + " " + reindeer[i];
-    console.log(data[i]);
-    console.log(data);
-    hohohoElement.innerHTML += data[i];
+    hohohoElement.innerHTML += "<div>"+ colors[i] + " " + reindeer[i] + "</div>";
+    console.log(hohohoElement);
+    
 };
 
 
